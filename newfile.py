f8e4689490d45b61256d25495851a8a230c54597
ISAghorbani <isaghorbano.2005@gmail.com>
2025-04-12 07:46:42 +0330

    Add files via upload
    علائم پر کاربرد ریاضی

---
+++ newfile.py
@@ -0,0 +1,31 @@
+a = 42
+b = 33
+c=a+b
+print(c)
+
+a = 23
+b = 53
+c=a-b
+print(c)
+
+a = 23
+b = 53
+c=a*b
+print(c)
+
+a = 23
+b = 53
+c=a/b
+print(c)
+
+a = 23
+b = 53
+c=a//b
+print(c)
+
+
+a = 23
+b = 53
+c=a**b
+print(c)
+
